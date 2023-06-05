52811f34c86f191a4efe7285d9d54d93ed6a4c7c
RAZAFINDRAKOTO Davidson Lova (SAFRAN AIRCRAFT ENGINES) <davidson-lova.razafindrakoto@safrangroup.com>
2023-06-05 09:45:09 +0200

    add resLin

--- tests/models/ABCSubSim.py
+++ tests/models/ABCSubSim.py
@@ -4,6 +4,25 @@ import torch.nn as nn
 import numpy as np
 
 # %%
+class resLin(nn.Module) :
+    def __init__(self, nin, nout) :
+        super().__init__()
+        self.nin = nin
+        self.nout = nout
+        self.Layer1 = nn.Linear(nin, nout)
+    def forward(self, tX) :
+        x = self.Layer1(tX)
+        return x
+    
+    def zero_grad(self, set_to_none: bool = False) -> None:
+        return super().zero_grad(set_to_none)
+    
+    def set_params(self, param_list):
+        for p1, p2 in zip(list(self.parameters()), param_list) :
+            p1.data = p2
+        return self
+
+# %%
 class resNet(nn.Module) :
     def __init__(self, nin, nout) :
         super().__init__()
