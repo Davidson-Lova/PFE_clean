25e1a99f84a9c66089e836351bc7a0fb4c40750f
RAZAFINDRAKOTO Davidson Lova (SAFRAN AIRCRAFT ENGINES) <davidson-lova.razafindrakoto@safrangroup.com>
2023-06-05 10:43:47 +0200

    change temp

--- tests/visualization/start.py
+++ tests/visualization/start.py
@@ -45,7 +45,7 @@ sigma_0 = (lmax + 1) * fact
 ns = [4,2,1]
 
 # Temperature initiale
-Temp = 1
+Temp = 10
 
 # Distance qui va être utiliser pour évaluer
 # la dissimilarité entre les prédictions y_hat et la réponse y
@@ -60,7 +60,7 @@ model = ABCSubSim.resNet(4,1)
 
 
 # %%
-indexPartition = 10
+indexPartition = 0
 ty = torch.Tensor(df.partitions[indexPartition][target_name].compute().values).reshape(-1,1)
 tyNorm =(ty - ty.mean(axis = 0)) / ty.std(axis = 0)
 tX = torch.Tensor(df.partitions[indexPartition][covariable_name].compute().values)
@@ -98,6 +98,62 @@ plt.fill_between(np.arange(ty.shape[0]), q25[:, 0], q75[:, 0],
 plt.plot(med, label="mediane", color="#8c0e11")
 
 plt.plot(tyNorm, label='N2_1', color = 'k')
+
+plt.legend()
+plt.show()
+
+# %%
+indexPartition = 10
+ty = torch.Tensor(df.partitions[indexPartition][target_name].compute().values).reshape(-1,1)
+tyNorm =(ty - ty.mean(axis = 0)) / ty.std(axis = 0)
+tX = torch.Tensor(df.partitions[indexPartition][covariable_name].compute().values)
+tXNorm = (tX - tX.mean(axis = 0)) / tX.std(axis = 0)
+
+
+# %%
+model = ABCSubSim.resNet(4,1)
+
+# %%
+y_hats = torch.concatenate(
+            tuple([model.set_params(theta).forward(tXNorm) for theta in thetas]), 1)
+q2_5 = y_hats.quantile(0.025, 1).reshape(-1,1).detach().numpy()
+q97_5 = y_hats.quantile(0.975, 1).reshape(-1,1).detach().numpy()
+q25 = y_hats.quantile(0.25, 1).reshape(-1,1).detach().numpy()
+q75 = y_hats.quantile(0.75, 1).reshape(-1,1).detach().numpy()
+med = y_hats.quantile(0.5, 1).reshape(-1,1).detach().numpy()
+plt.clf()
+plt.fill_between(np.arange(ty.shape[0]), q2_5[:, 0],
+                 q97_5[:, 0], color="#e8e8e8", label='95%')
+
+plt.fill_between(np.arange(ty.shape[0]), q25[:, 0], q75[:, 0],
+                 color="#bfbfbf", label='50%')
+
+plt.plot(med, label="mediane", color="#8c0e11")
+
+plt.plot(tyNorm, label='N2_1', color = 'k')
+
+plt.legend()
+plt.show()
+
+# %%
+residuals = y_hats - tyNorm
+q2_5 = residuals.quantile(0.025, 1).reshape(-1,1).detach().numpy()
+q97_5 = residuals.quantile(0.975, 1).reshape(-1,1).detach().numpy()
+q25 = residuals.quantile(0.25, 1).reshape(-1,1).detach().numpy()
+q75 = residuals.quantile(0.75, 1).reshape(-1,1).detach().numpy()
+med = residuals.quantile(0.5, 1).reshape(-1,1).detach().numpy()
+plt.clf()
+plt.fill_between(np.arange(ty.shape[0]), q2_5[:, 0],
+                 q97_5[:, 0], color="#e8e8e8", label='95%')
+
+plt.fill_between(np.arange(ty.shape[0]), q25[:, 0], q75[:, 0],
+                 color="#bfbfbf", label='50%')
+
+plt.hlines(0, xmin = 0, xmax = q2_5.shape[0], label = 'y = 0')
+
+plt.plot(med, label="mediane", color="#8c0e11")
+
+plt.ylim(-3, 3)
 
 plt.legend()
 plt.show()
