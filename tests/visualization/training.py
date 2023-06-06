31179ac931b275cf431f831fecbf58639bfc31f1
RAZAFINDRAKOTO Davidson Lova (SAFRAN AIRCRAFT ENGINES) <davidson-lova.razafindrakoto@safrangroup.com>
2023-06-06 09:48:02 +0200

    training over 7 concatenated flights and testing

--- tests/visualization/training.py
+++ tests/visualization/training.py
@@ -39,7 +39,7 @@ covariable_name = ["N1_1 [% rpm]",
 # %%
 # Hypperparamètres
 N = 5000
-lmax = 6
+lmax = 8
 P0 = 0.1
 epsilon = 0.01
 fact = 0.5
@@ -92,55 +92,139 @@ tyTest = torch.concatenate(tuple([torch.Tensor(df.partitions[index][target_name]
 
 
 # %%
-tXTrainNorm = (tXTrain - tXTrain.mean(axis=0)) / tXTrain.std(axis=0)
+tX = torch.concatenate((tXTrain, tXTest))
+ty = torch.concatenate((tyTrain, tyTest))
 
-tyTrainNorm = (tyTrain - tyTrain.mean(axis=0)) / tyTrain.std(axis=0)
+
+# Normalisation
+# %%
+tXTrainNorm = (tXTrain - tX.mean(axis=0)) / tX.std(axis=0)
+
+tyTrainNorm = (tyTrain - ty.mean(axis=0)) / ty.std(axis=0)
 
 # %%
-tXTestNorm = (tXTest - tXTest.mean(axis=0)) / tXTest.std(axis=0)
+tXTestNorm = (tXTest - tX.mean(axis=0)) / tX.std(axis=0)
 
-tyTestNorm = (tyTest - tyTest.mean(axis=0)) / tyTest.std(axis=0)
+tyTestNorm = (tyTest - ty.mean(axis=0)) / ty.std(axis=0)
 
 
 # %%
+# Training the BNN
 thetas, rhoMin, epsJ, rhoMax = ABCSubSim.trainBNN(args, tXTrainNorm, tyTrainNorm, model)
 
 # %%
+# BaseLine of linear regression
+aHat = torch.linalg.solve(tXTrainNorm.t() @ tXTrainNorm,
+                      tXTrainNorm.t() @ tyTrainNorm)
+yHat = np.matmul(tXTrainNorm, aHat)
+
+# %%
+# Error plots
+rhoFloor = torch.ones(lmax) *  (torch.cdist(tyTrainNorm.t(), yHat.t(), p=pdist) ** 2) / yHat.shape[0]
 plt.figure()
 plt.plot(np.float32(rhoMax), label = 'erreur Max', marker = '^')
 plt.plot(np.float32(epsJ), label = 'epsilon_j', marker = '>')
 plt.plot(np.float32(rhoMin), label = 'erreur Min', marker = 'v')
-# plt.plot(np.ones(len(rhoMin)) * epsilon, label = 'epsilon', marker = 'd')
+plt.plot(rhoFloor.t(), label = 'erreur RegLin', marker = 'd')
 plt.yscale('log')
 plt.legend()
 plt.grid('on')
 plt.show()
 
+# Evaluation of the error over the training set
+# %%
+rhoTrain = []
+for index in trainingIndices : 
+    tXloc = torch.Tensor(df.partitions[index][covariable_name].compute().values)
+    tXloc = (tXloc - tX.mean(axis = 0)) / tX.std(axis = 0)
+
+    tyLoc = torch.Tensor(df.partitions[index][target_name].compute().values).reshape(-1, 1)
+    tyLoc = (tyLoc - ty.mean(axis = 0)) / ty.std(axis = 0)
+    tYsLoc = torch.concatenate(
+                tuple([model.set_params(theta).forward(tXloc) for theta in thetas]), 1)
+    rhoLoc = (torch.cdist(tYsLoc.t(), tyLoc.t(), p=args["pdist"]) ** 2) / tyLoc.shape[0]
+    rhoTrain.append(rhoLoc.detach())
+
+
+# Evaluation of the error over the testing set
+# %%
+rhoTest = []
+for index in testingIndices : 
+    tXloc = torch.Tensor(df.partitions[index][covariable_name].compute().values)
+    tXloc = (tXloc - tX.mean(axis = 0)) / tX.std(axis = 0)
+
+    tyLoc = torch.Tensor(df.partitions[index][target_name].compute().values).reshape(-1, 1)
+    tyLoc = (tyLoc - ty.mean(axis = 0)) / ty.std(axis = 0)
+    tYsLoc = torch.concatenate(
+                tuple([model.set_params(theta).forward(tXloc) for theta in thetas]), 1)
+    rhoLoc = (torch.cdist(tYsLoc.t(), tyLoc.t(), p=args["pdist"]) ** 2) / tyLoc.shape[0]
+    rhoTest.append(rhoLoc.detach())
+
 
 # %%
-indexPartition = 100
-ty = torch.Tensor(df.partitions[indexPartition][target_name].compute().values).reshape(-1,1)
-tyNorm =(ty - ty.mean(axis = 0)) / ty.std(axis = 0)
-tX = torch.Tensor(df.partitions[indexPartition][covariable_name].compute().values)
-tXNorm = (tX - tX.mean(axis = 0)) / tX.std(axis = 0)
-
-y_hats = torch.concatenate(
-            tuple([model.set_params(theta).forward(tXNorm) for theta in thetas]), 1)
-q2_5 = y_hats.quantile(0.025, 1).reshape(-1,1).detach().numpy()
-q97_5 = y_hats.quantile(0.975, 1).reshape(-1,1).detach().numpy()
-q25 = y_hats.quantile(0.25, 1).reshape(-1,1).detach().numpy()
-q75 = y_hats.quantile(0.75, 1).reshape(-1,1).detach().numpy()
-med = y_hats.quantile(0.5, 1).reshape(-1,1).detach().numpy()
-plt.clf()
-plt.fill_between(np.arange(ty.shape[0]), q2_5[:, 0],
-                 q97_5[:, 0], color="#e8e8e8", label='95%')
-
-plt.fill_between(np.arange(ty.shape[0]), q25[:, 0], q75[:, 0],
-                 color="#bfbfbf", label='50%')
-
-plt.plot(med, label="mediane", color="#8c0e11")
-
-plt.plot(tyNorm, label='N2_1', color = 'k')
-
-plt.legend()
+plt.boxplot([ _.ravel() for _ in rhoTrain])
+plt.ylim(0, 0.4)
 plt.show()
+
+plt.boxplot([ _.ravel() for _ in rhoTest])
+plt.ylim(0, 0.4)
+plt.show()
+
+# %%
+# little test
+for indexPartition in trainingIndices :
+    ty = torch.Tensor(df.partitions[indexPartition][target_name].compute().values).reshape(-1,1)
+    tyNorm =(ty - ty.mean(axis = 0)) / ty.std(axis = 0)
+    tX = torch.Tensor(df.partitions[indexPartition][covariable_name].compute().values)
+    tXNorm = (tX - tX.mean(axis = 0)) / tX.std(axis = 0)
+
+    y_hats = torch.concatenate(
+                tuple([model.set_params(theta).forward(tXNorm) for theta in thetas]), 1)
+    q2_5 = y_hats.quantile(0.025, 1).reshape(-1,1).detach().numpy()
+    q97_5 = y_hats.quantile(0.975, 1).reshape(-1,1).detach().numpy()
+    q25 = y_hats.quantile(0.25, 1).reshape(-1,1).detach().numpy()
+    q75 = y_hats.quantile(0.75, 1).reshape(-1,1).detach().numpy()
+    med = y_hats.quantile(0.5, 1).reshape(-1,1).detach().numpy()
+    
+    plt.fill_between(np.arange(ty.shape[0]), q2_5[:, 0],
+                    q97_5[:, 0], color="#e8e8e8", label='95%')
+
+    plt.fill_between(np.arange(ty.shape[0]), q25[:, 0], q75[:, 0],
+                    color="#bfbfbf", label='50%')
+
+    plt.plot(med, label="mediane", color="#8c0e11")
+
+    plt.plot(tyNorm, label='N2_1', color = 'k')
+    plt.ylim(-4,1)
+    plt.legend()
+    plt.show()
+
+
+# %%
+for indexPartition in testingIndices :
+    ty = torch.Tensor(df.partitions[indexPartition][target_name].compute().values).reshape(-1,1)
+    tyNorm =(ty - ty.mean(axis = 0)) / ty.std(axis = 0)
+    tX = torch.Tensor(df.partitions[indexPartition][covariable_name].compute().values)
+    tXNorm = (tX - tX.mean(axis = 0)) / tX.std(axis = 0)
+
+    y_hats = torch.concatenate(
+                tuple([model.set_params(theta).forward(tXNorm) for theta in thetas]), 1)
+    q2_5 = y_hats.quantile(0.025, 1).reshape(-1,1).detach().numpy()
+    q97_5 = y_hats.quantile(0.975, 1).reshape(-1,1).detach().numpy()
+    q25 = y_hats.quantile(0.25, 1).reshape(-1,1).detach().numpy()
+    q75 = y_hats.quantile(0.75, 1).reshape(-1,1).detach().numpy()
+    med = y_hats.quantile(0.5, 1).reshape(-1,1).detach().numpy()
+
+    plt.fill_between(np.arange(ty.shape[0]), q2_5[:, 0],
+                    q97_5[:, 0], color="#e8e8e8", label='95%')
+
+    plt.fill_between(np.arange(ty.shape[0]), q25[:, 0], q75[:, 0],
+                    color="#bfbfbf", label='50%')
+
+    plt.plot(med, label="mediane", color="#8c0e11")
+
+    plt.plot(tyNorm, label='N2_1', color = 'k')
+    plt.ylim(-4,1)
+    plt.legend()
+    plt.show()
+
