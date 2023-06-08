14e568eb44c919603ad63a1e681e45a35b3f9db2
RAZAFINDRAKOTO Davidson Lova (SAFRAN AIRCRAFT ENGINES) <davidson-lova.razafindrakoto@safrangroup.com>
2023-06-08 10:35:02 +0200

    normalisation

--- src/visualization/trainingNet.py
+++ src/visualization/trainingNet.py
@@ -14,15 +14,10 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath("models")), '..'))
 from models import ABCSubSim
 from models import rotTube
 # %%
-
-# %%
 # Here we need to able to extract other flight data from
 # the .h5 file, train the model over one flight
 # And trying to use the same model for predictions
 # for other flights
-
-
-# %%
 
 # %%
 file_name = "../../data/raw/Aircraft_01_dask.h5"
